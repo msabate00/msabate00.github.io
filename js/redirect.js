d7fa0f3b82625e899d4abda7003663c22c70daf4
Marti Sabaté <sabatemiranda@gmail.com>
2026-08-10 19:55:23 +0200

    redirecto from github.io to martisabate.dev

---
+++ js/redirect.js
@@ -0,0 +1,8 @@
+if (window.location.hostname === "msabate00.github.io") {
+  window.location.replace(
+    "https://martisabate.dev" +
+    window.location.pathname +
+    window.location.search +
+    window.location.hash
+  );
+}
